66d109a81d037a5a64a0199726bfc6b67c8c9e83
AryanAg08 <aryangoyal90@outlook.com>
2022-12-11 22:15:11 +0530

    V1.2

--- src/Features/features/11Study_vibes.js
+++ src/Features/features/11Study_vibes.js
@@ -10,21 +10,22 @@ module.exports = (client) => {
             let count = 0;
 
             for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
-            console.log(count);
-
+            
             const channelId = "886948093566808095";
 
             const AA = await guild.channels.cache.get(channelId);
             AA.setName(`Currently Studying: ${count}`);
-
-            MMC = guild.memberCount
-            const CHANID = "876034122164895745"
-            const CHANNN = guild.channels.cache.get(CHANID);
-            const TC = MMC / 1000 
-            const VD = Math.round(TC * 100 ) / 100 
-            console.log(TC)
-            CHANNN.setName(`Total Members: ${VD}k`);
             
+            let memberCount;
+            memberCount = guild.roles.cache.get("707547622591692911").members.size;
+            const channelID = "862013407225053244"
+            const channel = guild.channels.cache.get(channelID)
+            console.log(memberCount);
+            const CC = memberCount / 1000 
+            const NN = Math.round(CC * 100) / 100
+            channel.setName(`Verified: ${NN}k`)
+            
+
         });
 
     })
