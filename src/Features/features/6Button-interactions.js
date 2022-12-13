9ef12a01e78c1e06857f6e91a47443ffd70d425f
AryanAg08 <aryangoyal90@outlook.com>
2022-12-13 10:46:24 +0530

    V1.2

--- src/Features/features/6Button-interactions.js
+++ src/Features/features/6Button-interactions.js
@@ -2,19 +2,19 @@ const { help1, help2 } = require("../../functions/4Buttons");
 
 module.exports = (client) => {
 
-    client.on('interactionCreate', async interaction => {
-        if (!interaction.isButton()) return;
+    // client.on('interactionCreate', async interaction => {
+    //     if (!interaction.isButton()) return;
 
-        else {
+    //     else {
             
 
-            if (interaction.customId === "HPx001") {
-                help1(interaction);
-            }
+    //         if (interaction.customId === "HPx001") {
+    //             help1(interaction);
+    //         }
 
-            if (interaction.customId === "HPx002") {
-               help2(interaction);
-            }
-        }
-    })
+    //         if (interaction.customId === "HPx002") {
+    //            help2(interaction);
+    //         }
+    //     }
+    // })
 }
