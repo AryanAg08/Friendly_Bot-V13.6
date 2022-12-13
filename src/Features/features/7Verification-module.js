3427e3d77dae4cc43d8bbcdc4943526bb4fc27d4
AryanAg08 <aryangoyal90@outlook.com>
2022-12-13 19:12:57 +0530

    V1.2

--- src/Features/features/7Verification-module.js
+++ src/Features/features/7Verification-module.js
@@ -86,11 +86,100 @@ module.exports  = (client) => {
             if (N1) {
                 for (oo of N1) {
                     const CHAN = oo.VerificationChan
-                    const Role = oo.VerificationRole
                     const MSgChan = oo.VerificationMsgchannel
+                    
+                    if (message.channel.id === CHAN) {
+                        let mssg = message.content;
+
+                        const CHannel = client.channels.cache.get(MSgChan);
+                        if (CHannel) {
+                            message.author.send(`Thank you for sending in your verification form! One of the mods will check your answer and verify you for the server. We wish you a nice day and a good stay in the server. \n\n **${message.guild.name}**`)
+                            .catch(() => console.log(`Can't send this in DM!!`));
+                              
+                            const { MessageEmbed,  MessageActionRow, MessageButton } = require("discord.js");
+                            const Embed = new MessageEmbed()
+                            .setColor("RANDOM")
+                            .setDescription(`<@${message.author.id}> Wants to get verified!! \n ${message.content}`)
+                            .setTimestamp()
+
+                            const row = new MessageActionRow()
+                            .addComponents(
+                                new MessageButton()
+                                .setCustomId(`AC${message.author.id}`)
+                                .setLabel("Accept")
+                                .setStyle("SUCCESS"),
+
+                                new MessageButton()
+                                .setCustomId(`DN${message.author.id}`)
+                                .setLabel("Deny")
+                                .setStyle("DANGER")
+                            )
+                      
+                            
+                            const Gd = await CHannel.send({
+                                embeds: [Embed],
+                                components: [row],
+                            });
+
+                            const ID = Gd.id
+
+                            const L1 = await v2.findOneAndUpdate({
+                                GG: GG,
+                                user: user,
+                            },{
+                                user: user,
+                                VerificationStatus: "WAITING",
+                                MSGID: ID,
+                            },{
+                                upsert: true,
+                                new: true,
+                            })
+                            
+                        }
+                        message.delete();
+                    }
+
                 }
             }
 
         } else return;
     });
+
+    client.on("interactionCreate", async (interaction) => {
+        if (!interaction.isButton()) return;
+      const Guild = interaction.guild.id;
+      const BID = interaction.customId;
+      
+              const K1 = await v1.find({
+                GuildID: Guild,
+                Verification: "YES",
+              })
+              if (K1) {
+                const K2 = await v2.find({
+                    GG: Guild,
+                    MSGID: interaction.id,
+                    VerificationStatus: "WAITING",
+                })
+                if (K2) {
+                    for (tt of K2) {
+                        const Role = tt.VerificationRole
+                        const auth = tt.user
+
+                        if (BID === `AC${auth}`) {
+                            interaction.guild.members.cache.get(auth).roles.add(Role);
+                            const USer = interaction.guild.members.cache.get(auth);
+
+                            USer.send(" Hi! A mod just verified you for access on the Study Vibes server. \nWe wish you good luck with your studies and a happy stay! \n Don't forget to have a look at the toolbox-channel to unlock different toolbox-clubs!")
+                            .catch(() => console.log("Can't send this user dm!!"));
+                        }
+                        if (BID === `DN${auth}`) {
+                            const USer = interaction.guild.members.cache.get(auth);
+
+                            USer.send("Hi! Unfortunately you did not pass the verification process for our server. If you think this was a mistake feel free to contact an available mod. We wish you all the best. ")
+                                .catch(() => console.log("Can't send this user dm!!"))
+                    } 
+                }
+            }
+        }
+    })
 }
