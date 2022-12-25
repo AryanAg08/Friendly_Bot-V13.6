b4fab8c7b4a74b2c2b7d7fd9250761bd12ae2b14
AryanAg08 <aryangoyal90@outlook.com>
2022-12-26 01:15:15 +0530

    V1.1.1

--- src/Commands/commands/SV_CMDS/Christmas Module/5sendwishing.js
+++ src/Commands/commands/SV_CMDS/Christmas Module/5sendwishing.js
@@ -5,15 +5,15 @@ module.exports = {
         const { author, guild } = message;
 
         if (guild.id === "703937875720273972") {
-            guild.members
-             .fetch()
-              .then(members => members.forEach(member => {
-                  member
+            guild.members.cache.fetch("125438257654792192")
+             //.fetch()
+              //.then(members => members.forEach(member => {
+                 // member
             .send(`Merry Christmas <@${member.id}>⛄!!\n I hope you day was well spent and you enjoyed it a lot. I wish you all the best and send my heartmost thank you for being a part of the study vibes community. Thank you for joining our little family and making it more awesome. I wish you achieve all your dreams and become successful in your life and bring smile to yourself and many other on your way towards your success. \n I hope your 2022 was cheering at some point. (if not please come share it with me <@${author.id}> I am eagerly waiting to hear from each one of you.). \n I wish from all of my heart that this upcoming 2023 will bring you atmost joy and happiness and you achieve all the happiness that you deserve. \n With this imma end my message. (But that doesn't mean I have no words to say. :p). \n Merry christmas!! And also a happy happy new year 🌟✨✨🎆🎆❄️☃️⛄`)
-            .catch(() => {
-                console.error(`Failed to send ${member.user.tag} a message`)
-            })
-    }))
+            //.catch(() => {
+              //  console.error(`Failed to send ${member.user.tag} a message`)
+            //})
+   // }))
         }
     }
 }
