2fcb0dcc6fb8daa09d264d4bdff037647b309c00
AryanAg08 <aryangoyal90@outlook.com>
2023-01-01 15:31:00 +0530

    V1.1.4

--- src/Features/features/6Button-interactions.js
+++ src/Features/features/6Button-interactions.js
@@ -2,19 +2,55 @@ const { help1, help2 } = require("../../functions/4Buttons");
 
 module.exports = (client) => {
 
-    // client.on('interactionCreate', async interaction => {
-    //     if (!interaction.isButton()) return;
+    client.on('interactionCreate', async interaction => {
+        if (!interaction.isButton()) return;
 
-    //     else {
+        else {
             
 
-    //         if (interaction.customId === "HPx001") {
-    //             help1(interaction);
-    //         }
+            if (interaction.customId === "HPx001") {
+                help1(interaction);
+            }
 
-    //         if (interaction.customId === "HPx002") {
-    //            help2(interaction);
-    //         }
-    //     }
-    // })
+            if (interaction.customId === "HPx002") {
+               help2(interaction);
+            }
+
+
+         
+            const Guild = interaction.guild.id
+            const BID = interaction.customId;
+            const userId = interaction.user.id;
+               if (BID === `PVC${userId}`) {
+
+               
+            const L1 = require("../../models/18Private_channels");
+
+            const L2 = await L1.find({
+                GG: Guild,
+                user: userId,
+            })
+
+            if (L2) {
+                for (ff of L2) {
+                    const VCC = ff.VC
+                    const type = ff.Type
+                    const TCC = ff.TC
+
+                    if (type === "BOTH") {
+                       const vchan = interaction.guild.channels.cache.get(VCC);
+                       const tchan = interaction.guild.channels.cache.get(TCC);
+
+                       if (vchan) {
+                        vchan.delete();
+                       }
+                       if (tchan) {
+                        tchan.delete();
+                       }
+                    }
+                }
+            } else return;
+        }
+    }
+    })
 }
