d26c798a12097b3a928adac523936b12b3637459
AryanAg08 <aryangoyal90@outlook.com>
2023-03-30 15:45:37 +0530

    V1.2.1

--- src/Features/features/4Welcome_module.js
+++ src/Features/features/4Welcome_module.js
@@ -14,7 +14,7 @@ module.exports = (client) =>  {
        if (Guild === "1088582075763925053") {
            const embed = new MessageEmbed()
            .setColor("RANDOM")
-           .setImage("https://imgur.com/a/4Pp003W")
+           .setImage("https://cdn.discordapp.com/attachments/1090928183274123285/1090942214890332190/ezgif.com-video-to-gif.gif")
            .setDescription(`Welcome ${Tag} to the server \n JIIT OPTICA presents **Code Of Duty**.`)
 
            const channel = client.channels.cache.get("1090928183274123285");
