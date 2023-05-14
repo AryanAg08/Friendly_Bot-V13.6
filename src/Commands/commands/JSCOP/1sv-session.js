36871fe5499a6675f287ef10eb0a0d5b54ddc6f8
AryanAg08 <aryangoyal90@outlook.com>
2023-05-14 16:52:07 +0530

    event

--- src/Commands/commands/JSCOP/1sv-session.js
+++ src/Commands/commands/JSCOP/1sv-session.js
@@ -7,10 +7,9 @@ module.exports = {
         const X2 =  await X1.find()
           var reply = "Total Study Time Today!! \n\n"
 
-       
+          X2.sort((a, b) => b.timestamp - a.timestamp);
             X2.forEach(QQ => {
                 if (QQ.timestamp != 0 && QQ.timestamp != undefined) {
-                   QQ.sort((a, b) => b.timestamp - a.timestamp);
                     reply += `<@${QQ.user}> -> ${QQ.timestamp} minutes\n`;
                 }                 
             });
